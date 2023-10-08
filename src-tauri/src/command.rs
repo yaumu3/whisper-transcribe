5ae56fea034bdefeb924eb30259dd8f85da5d6c0
yaumu3 <yumu@me.com>
2023-10-08 23:36:44 +0900

    fix: async within async

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -7,22 +7,21 @@ pub fn set_api_key_to_config_file(api_key: &str) -> Result<(), String> {
     }
 }
 
-#[tauri::command(async)]
-pub fn post_to_whisper(lang: &str, file_path: &str) -> Result<String, String> {
+#[tauri::command]
+pub async fn post_to_whisper(lang: &str, file_path: &str) -> Result<String, String> {
     let api_key = match crate::config::get_api_key() {
         Ok(Some(api_key)) => api_key,
         Ok(_) => return Err("No API key found in config file".to_owned()),
         Err(e) => return Err(e.to_string()),
     };
-    let response = match crate::api::post(&api_key, lang, file_path) {
-        Ok(response) => response,
-        Err(e) => return Err(e.to_string()),
-    };
-    Ok(response)
+    tokio::task::block_in_place(move || match crate::api::post(&api_key, lang, file_path) {
+        Ok(response) => Ok(response),
+        Err(e) => Err(e.to_string()),
+    })
 }
 
-#[tauri::command(async)]
-pub fn save_transcription(transcription: &str, save_file_path: &str) -> Result<(), String> {
+#[tauri::command]
+pub async fn save_transcription(transcription: &str, save_file_path: &str) -> Result<(), String> {
     match std::fs::write(save_file_path, transcription) {
         Ok(_) => Ok(()),
         Err(e) => Err(e.to_string()),
